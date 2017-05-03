72e33c2b01fabe4149827173a89c0a791c7c8412
root <root@waite.io>
2017-05-02 22:32:50 -0400

    revert

--- index.js
+++ index.js
@@ -12,9 +12,11 @@ app.use(express.static("public/homepage"));
 app.get('/resume', function(req,res) {
 	res.sendFile('public/LatexResume/src/resume.pdf', {root: __dirname});
 });
-app.post('/github-prhook-resume', function(req,res) {
-        pullNewResume = exec('(cd public/LatexResume && git pull)', function (error, stdout, stderr) {});
-});
+//app.post('/github-prhook-resume', function(req,res) {
+//	console.log("hit");
+//        pullNewResume = exec('echo HIT', function (error, stdout, stderr) {});
+//});
+//(cd public/LatexResume && git pull)
 var server = app.listen(80, function () {
 	var host = server.address().address; 
 	var port = server.address().port;
