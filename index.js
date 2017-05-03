6e5371b06b46cb871ca252f0caa19f5ac5978423
Joseph Waite <waitejg@mcmaster.ca>
2017-05-02 22:06:01 -0400

    added a build hook

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+var exec = require('child_process').exec;
+var pullNewResume; 
 var express = require('express');
 var app = express();
+
+pullNewResume = exec('(cd public/LatexResume && git pull)',
+  function (error, stdout, stderr) {
+});
+
 app.use(express.static("public/homepage"));
+
 app.get('/resume', function(req,res) {
 	res.sendFile('public/LatexResume/src/resume.pdf', {root: __dirname});
+});
+app.post('/github-prhook-resume', function(req,res) {
+        pullNewResume = exec('(cd public/LatexResume && git pull)', function (error, stdout, stderr) {});
 });
 var server = app.listen(80, function () {
 	var host = server.address().address; 
